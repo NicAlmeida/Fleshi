ccdea9ffb6e4c8c9c13df07e9d167e75bb287d32
NicAlmeida <nicolly.almeida.nicky@gmail.com>
2025-12-09 16:40:45 -0300

    build: update init with postgres

--- appflexi/__init__.py
+++ appflexi/__init__.py
@@ -1,11 +1,21 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dataflexi.db'
-app.config['SECRET_KEY'] = 'secret'
+
+
+database_url = os.getenv('DATABASE_URL_INTERNAL')
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///dataflexi.db'
+
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret')
 app.config['UPLOAD_FOLDER'] = 'static/posts_photos'
 
 database = SQLAlchemy(app)
